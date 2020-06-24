98fb3f6c3ad132001f2ab0d474e59a3d888e8e39
zhidaothecoder <46723925+zhidaothecoder@users.noreply.github.com>
2020-06-24 18:55:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', msg => {
     const args = msg.content.split(' ').slice(1).join(' ');
     if (command === 'guide') return msg.channel.send('https://github.com');
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
-    else if (command === 'info') return msg.channel.send("***geez. you're nosy tryna find out more about me. but whatever.***I am a multi-purpose discord bot made by Zhidao. I guess if you want a bot he ***might*** help. I wouldn't ask him now, he's coding.");
+    else if (command === 'info') return msg.channel.send("***geez. you're nosy tryna find out more about me. but whatever.*** I am a multi-purpose discord bot made by Zhidao. I guess if you want a bot he ***might*** help. I wouldn't ask him now, he's coding.");
     else if (command === 'prefix') return msg.channel.send("My prefix is:'z!'");
 });
 
