fe417d37ef3eb1c7d75a6df08bd62553bfe3b77c
zhidaothecoder <46723925+zhidaothecoder@users.noreply.github.com>
2020-06-24 19:50:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on('message', msg => {
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
     else if (command === 'info') return msg.channel.send("***geez. you're nosy tryna find out more about me.*** I am a multi-purpose discord bot made by Zhidao. I guess if you want a bot he ***might*** help. I wouldn't ask him now, he's coding.");
     else if (command === 'prefix') return msg.channel.send("My prefix is: `z!`");
-    else if (command === 'server') return msg.channel.send("This server is called: "{message.guild.name});
+    else if (command === 'server') return message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
 });
 
 client.login(process.env.BOT_TOKEN);
