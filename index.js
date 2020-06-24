e13e1f90824a36c7cb0b524c5deb79f99a6d0c36
zhidaothecoder <46723925+zhidaothecoder@users.noreply.github.com>
2020-06-24 18:31:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('Zhidao code me', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
