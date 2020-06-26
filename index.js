7598d13ee0aad6830fb1198b6192998569a6e094
zhidaothecoder <46723925+zhidaothecoder@users.noreply.github.com>
2020-06-26 13:11:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setActivity('Zhidao code me | zb!info | zb!prefix', {type: 'WATCHING'});
+    
 });
 
 client.on('message', msg => {
@@ -14,7 +15,22 @@ client.on('message', msg => {
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
     else if (command === 'info') return msg.channel.send("***geez. you're nosy tryna find out more about me.*** I am a multi-purpose discord bot made by Zhidao. I guess if you want a bot he ***might*** help. I wouldn't ask him now, he's coding.");
     else if (command === 'prefix') return msg.channel.send("My prefix is: `zb!`");
-    else if (command === 'code')  msg.channels.find(x => x.name === 'coder').send("Hello <@&705053625113903104>s, time to code! Dm Zhidao or <#722169162658611220>.");
+});
+
+client.on('message', function(msg){
+    if (msg.content === 'zb!code') {
+     client.channels.find(x => x.name === 'coders').send('Time to code!', {
+  	tts: true
+      });
+  }
+  console.log(msg.author.username);
+})
+
+client.on('ready', function(){
+  console.log("Bot is now connected");
+  client.user.setActivity('Zhidao code me | zb!info | zb!prefix', {type: 'WATCHING'});
+  client.channels.find(x => x.name === 'coders').send("Ready and online!");
+  
 });
 
 client.login(process.env.BOT_TOKEN);
